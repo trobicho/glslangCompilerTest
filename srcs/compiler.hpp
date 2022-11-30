21797e5bbfa86b0e3a2bac1d3aad0d9a9f5dfec4
trobicho <tomtomy35@gmail.com>
2022-11-30 18:12:52 +0100

    first commit

---
+++ srcs/compiler.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <glslang/Public/ShaderLang.h>
+
+class GlslCompiler {
+  public:
+    GlslCompiler() {};
+};
---
+++ srcs/main.cpp
@@ -0,0 +1,8 @@
+#include "compiler.hpp"
+#include <iostream>
+
+int main(int ac, char **av) {
+  GlslCompiler  compiler;
+
+  return (EXIT_SUCCESS);
+}
